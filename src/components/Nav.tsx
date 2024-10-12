84fdfb54a4eca053ec88c7b881b68757c5f6a6a2
Jean Christoffer <89041011+Jean-Christoffer@users.noreply.github.com>
2024-10-12 18:20:39 +0200

    projects

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,8 +2,9 @@
 
 export default function Nav() {
   return (
-    <header className="p-8 w-fit absolute z-10 left-0 top-0">
-      <p>Jean Dev</p>
+    <header className="p-8 w-fit absolute z-10 left-0 top-0 flex items-center gap-4">
+      <button>Projects</button>
+      <button>Contact</button>
     </header>
   );
 }
